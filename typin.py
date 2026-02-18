4798a9bd68ea7d741db3deff422da7003c3a2006
Matchacha <morganevanreybrouck7@gmail.com>
2026-02-18 14:14:59 -0400

    eh idont know just wantws to see

--- typin.py
+++ typin.py
@@ -1,7 +1,7 @@
 import random 
 import time
 
-def get_words(niveles)
+def get_words(nivel)
     facil = [
         "instituto", "salchicha", "guanabana", "caracol", "ortopeda", "institucion"
     ]
@@ -11,3 +11,18 @@ def get_words(niveles)
     dificil = [
         "Idiosincrasia", "Otorrinolaringólogo", "Psicopedagogía", "Transterritorialidad", 
     ]
+
+if nivel == "facil": 
+    return facil
+elif nivel == "medio":
+    return medio
+else:
+    return dificil
+
+def typing_game():
+    nivel = input("elige nivel (facil/medio/dificil)")
+    words = get_words(nivel)
+    random.shuffle(words)
+    total_words = len(words)
+    mistakes = 0
+    
