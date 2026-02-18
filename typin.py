f0edda8321f1d640125ae13c0d6ceaa3e17f6e9d
Matchacha <morganevanreybrouck7@gmail.com>
2026-02-18 13:34:56 -0400

    random words

--- typin.py
+++ typin.py
@@ -1,1 +1,13 @@
-im
+import random 
+import time
+
+def get_words(niveles)
+    facil = [
+        "instituto", "salchicha", "guanabana", "caracol", "ortopeda", "institucion"
+    ]
+    medio = [
+        "circustancia", "anfitrion", "teatral", "romanticismo", "carambola", "diferenciacion"
+    ]
+    dificil = [
+        "Idiosincrasia", "Otorrinolaringólogo", "Psicopedagogía", "Transterritorialidad", 
+    ]
