fcb2c97ccf6df833daca237a2fccdfec75954b1a
Matchacha <morganevanreybrouck7@gmail.com>
2026-02-18 23:11:41 -0400

    edit

--- typin.py
+++ typin.py
@@ -1,23 +1,26 @@
 import random 
 import time
 
-def get_words(nivel)
+def get_words(nivel):
     facil = [
-        "instituto", "salchicha", "guanabana", "caracol", "ortopeda", "institucion"
+        "instituto", "salchicha", "guanabana", 
+        "caracol", "ortopeda", "institucion"
     ]
     medio = [
-        "circustancia", "anfitrion", "teatral", "romanticismo", "carambola", "diferenciacion"
+        "circustancia", "anfitrion", "teatral", 
+        "romanticismo", "carambola", "diferenciacion"
     ]
     dificil = [
-        "Idiosincrasia", "Otorrinolaringólogo", "Psicopedagogía", "Transterritorialidad", 
+        "Idiosincrasia", "Otorrinolaringólogo", 
+        "Psicopedagogía", "Transterritorialidad", 
     ]
-
-if nivel == "facil": 
-    return facil
-elif nivel == "medio":
-    return medio
-else:
-    return dificil
+    
+    if nivel == "facil": 
+        return facil
+    elif nivel == "medio":
+        return medio
+    else:
+        return dificil
 
 def typing_game():
     nivel = input("elige nivel (facil/medio/dificil)")
@@ -31,9 +34,29 @@ def typing_game():
 for i, word in enumerate(words, start=1): ## i es el índice, word es la palabra
     print("Palabra {}/{}: {}".format(i, total_words, word)) # Muestra progreso y palabra actual
     user_input = input("escrbe la palabra: ") 
-    if user_input.strip().lower() == word.lower()
+    if user_input.strip().lower() == word.lower():
         print("Correcto\n")
     else:
+        print("incorrecto\n")
+        mistake +=1
+    time.lapse(1)
 
+correct = total_words - mistakes
+accuracy = (correct / total_words)
 
+print("\n Fin del juego")
+print("nivel jugado:", nivel.capitalize())
+print("Palabras totales:", total_words)
+print("Errores cometidos:", mistakes)
+print("Palabras correctas:", correct)
+print("Tu precisión fue de: {:.2f}%".format(accuracy))
 
+if accuracy == 100: 
+    print(" No cometiste ningún error")
+elif accuracy >= 70: 
+    print("sigue practicando para mejorar aun mas que esto esta miserable")
+else: 
+    print("ay que verguenza")
+
+if __name__ == "__main__":
+    typing_game()
