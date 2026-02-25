71f6667bace4ad16e1082853cad98ee17e9f2bde
Matchacha <morganevanreybrouck7@gmail.com>
2026-02-25 17:02:33 -0400

    it doesnt let me type i need help

--- typin.py
+++ typin.py
@@ -1,4 +1,4 @@
-import random 
+import random
 import time
 
 def get_words(nivel):
@@ -12,7 +12,7 @@ def get_words(nivel):
     ]
     dificil = [
         "Idiosincrasia", "Otorrinolaringólogo", 
-        "Psicopedagogía", "Transterritorialidad", 
+        "Psicopedagogía", "Transterritorialidad"
     ]
     
     if nivel == "facil": 
@@ -23,40 +23,40 @@ def get_words(nivel):
         return dificil
 
 def typing_game():
-    nivel = input("elige nivel (facil/medio/dificil)")
+    nivel = input("Elige nivel (facil/medio/dificil): ")
     words = get_words(nivel)
     random.shuffle(words)
     total_words = len(words)
     mistakes = 0
     
-    print("/n Juego de rapidez en escritura lol")
-    print("escribe rapidamente la palabra que te aparece abajito ahi")
-    for i, word in enumerate(words, start=1): ## i es el índice, word es la palabra
-        print("Palabra {}/{}: {}".format(i, total_words, word)) # Muestra progreso y palabra actual
-        user_input = input("escrbe la palabra: ") 
+    print("\nJuego de rapidez en escritura")
+    print("Escribe rápidamente la palabra que aparece abajo")
+    for i, word in enumerate(words, start=1):
+        print("Palabra {}/{}: {}".format(i, total_words, word))
+        user_input = input("Escribe la palabra: ") 
         if user_input.strip().lower() == word.lower():
             print("Correcto\n")
         else:
-            print("incorrecto\n")
-        mistake +=1
-    time.lapse(1)
+            print("Incorrecto\n")
+            mistakes += 1
+        time.sleep(1)
 
     correct = total_words - mistakes
-    accuracy = (correct / total_words)
+    accuracy = (correct / total_words) * 100
 
-    print("\n Fin del juego")
-    print("nivel jugado:", nivel.capitalize())
+    print("\nFin del juego")
+    print("Nivel jugado:", nivel.capitalize())
     print("Palabras totales:", total_words)
     print("Errores cometidos:", mistakes)
     print("Palabras correctas:", correct)
-    print("Tu precisión fue de: {:.2f}%".format(accuracy)) #muestra con dos decimales
+    print("Tu precisión fue de: {:.2f}%".format(accuracy))
 
     if accuracy == 100: 
-        print(" No cometiste ningún error")
+        print("No cometiste ningún error")
     elif accuracy >= 70: 
-        print("sigue practicando para mejorar aun mas que esto esta miserable")
+        print("¡Bien! Sigue practicando para mejorar aún más")
     else: 
-        print("ay que verguenza")
+        print("Ay, qué vergüenza")
 
-if __name__ == "__main__": #para q se ejecute directamente 
+if __name__ == "__main__": 
     typing_game()
